2c9c3db0309eb91f80ad63feab4d2e445f08a886
avanwyk <abvanwyk@gmail.com>
2016-04-18 11:15:40 +0200

    Add Random for PRNG encapsulation
    Add unit tests in RandomTest. Random encapsulates a PRNG generator and
    useful distributions with interface for creating VectorXds.

---
+++ cswarm/cc/Random.cc
@@ -0,0 +1,35 @@
+/*
+Copyright 2016 Andrich van Wyk
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "Random.h"
+
+using Eigen::VectorXd;
+
+Random::Random(int64_t seed): generator(seed) { }
+
+VectorXd Random::randomVector(int size) {
+  return VectorXd::NullaryExpr(size, [this] (VectorXd::Index) {
+    return uniform(generator);
+  });
+}
+
+VectorXd Random::randomVector(int size, double lower, double upper) {
+  std::uniform_real_distribution<> distribution(lower, upper);
+  
+  return VectorXd::NullaryExpr(size, [this, &distribution] (VectorXd::Index) {
+    return distribution(generator);
+  });
+}
---
+++ cswarm/cc/Random.h
@@ -0,0 +1,36 @@
+/*
+Copyright 2016 Andrich van Wyk
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef CSWARM_CC_RANDOM_H_
+#define CSWARM_CC_RANDOM_H_
+
+#include <stdint.h>
+#include <random>
+#include <Eigen/Core>
+
+class Random {
+ public:
+  explicit Random(int64_t);
+  ~Random() {}
+  Eigen::VectorXd randomVector(int size);
+  Eigen::VectorXd randomVector(int size, double lower, double upper);
+
+ private:
+  std::mt19937_64 generator;
+  std::uniform_real_distribution<> uniform;
+};
+
+#endif  // CSWARM_CC_RANDOM_H_
